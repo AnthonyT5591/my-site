3673bfd4646f0f0115e3bd65c69e6f0cedd20e76
AnthonyT5591 <17280861+AnthonyT5591@users.noreply.github.com>
2022-05-01 16:50:49 -0700

    error handling in case of bad request

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -48,7 +48,7 @@ export default class Gallery extends Component {
     }
     renderGallery() {
         let temp = [];
-        if (this.state.gallery.length > 0) {
+        if (this.state.gallery?.length > 0) {
             this.state.gallery.forEach((c, i) => {
                 temp.push(
                     <img key={i}
@@ -58,6 +58,10 @@ export default class Gallery extends Component {
                         }} src={c.images.fixed_height_small.url} />
                 )
             })
+        } else {
+            temp.push(
+                <h2>Yikes! Someone was out late last night. Sorry!</h2>
+            )
         }
 
         return temp;
